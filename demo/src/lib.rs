e450978720ec147a4aebf763990267eda3bd77f5
NYBACHOK <nybachokua@gmail.com>
2024-11-10 15:07:19 +0200

    add prometheus

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -3,11 +3,14 @@ mod server;
 
 pub use client::*;
 pub use server::*;
+use std::sync::OnceLock;
 
 use opentelemetry::trace::TracerProvider;
 use opentelemetry::KeyValue;
 use opentelemetry_appender_tracing::layer::OpenTelemetryTracingBridge;
 use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::metrics::reader::DefaultTemporalitySelector;
+use opentelemetry_sdk::metrics::{PeriodicReader, SdkMeterProvider};
 use opentelemetry_sdk::propagation::TraceContextPropagator;
 use opentelemetry_sdk::trace::Config;
 use opentelemetry_sdk::Resource;
@@ -36,14 +39,21 @@ pub fn init_tracing() -> anyhow::Result<()> {
         )
         .install_batch(opentelemetry_sdk::runtime::Tokio)?;
 
-    let tracing_telemetry = tracing_opentelemetry::layer().with_tracer(tracer.tracer("opentelemetry"));
+    let tracing_telemetry =
+        tracing_opentelemetry::layer().with_tracer(tracer.tracer("opentelemetry"));
 
-    let logging = opentelemetry_otlp::new_pipeline().logging().with_exporter(opentelemetry_otlp::new_exporter()
-        .tonic()
-        .with_endpoint(&endpoint)).with_resource(Resource::new(vec![KeyValue::new(
-        opentelemetry_semantic_conventions::resource::SERVICE_NAME,
-        "rust-basic-app",
-    )])).install_batch(opentelemetry_sdk::runtime::Tokio)?;
+    let logging = opentelemetry_otlp::new_pipeline()
+        .logging()
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_endpoint(&endpoint),
+        )
+        .with_resource(Resource::new(vec![KeyValue::new(
+            opentelemetry_semantic_conventions::resource::SERVICE_NAME,
+            "rust-basic-app",
+        )]))
+        .install_batch(opentelemetry_sdk::runtime::Tokio)?;
 
     let logging_telemetry = OpenTelemetryTracingBridge::new(&logging);
 
@@ -53,6 +63,32 @@ pub fn init_tracing() -> anyhow::Result<()> {
         .with(logging_telemetry)
         .with(tracing_subscriber::EnvFilter::from_default_env())
         .init();
-    
+
     Ok(())
+}
+
+pub fn metering_provider() -> &'static SdkMeterProvider {
+    static METER_PROVIDER: OnceLock<SdkMeterProvider> = OnceLock::new();
+
+    METER_PROVIDER.get_or_init(|| {
+        let exporter = opentelemetry_otlp::new_exporter()
+            .tonic()
+            .with_endpoint(
+                std::env::var("OTEL_EXPORTER_OTEL_URL")
+                    .ok()
+                    .unwrap_or("http://localhost:4317".to_owned()),
+            )
+            .build_metrics_exporter(Box::new(DefaultTemporalitySelector::new()))
+            .expect("failed to build metrics exporter");
+        let reader = PeriodicReader::builder(exporter, opentelemetry_sdk::runtime::Tokio).build();
+        let provider = SdkMeterProvider::builder()
+            .with_reader(reader)
+            .with_resource(Resource::new([KeyValue::new(
+                "service.name",
+                "metrics-basic-example",
+            )]))
+            .build();
+        opentelemetry::global::set_meter_provider(provider.clone());
+        provider
+    })
 }
--- demo/src/server.rs
+++ demo/src/server.rs
@@ -1,6 +1,8 @@
 use std::time::Duration;
 
+use crate::metering_provider;
 use axum::{http::StatusCode, routing::get, Router};
+use opentelemetry::metrics::MeterProvider;
 use tracing::Level;
 
 pub async fn run_sample_server() -> anyhow::Result<()> {
@@ -19,9 +21,17 @@ async fn handler() -> Result<(), StatusCode> {
     ))
     .await;
 
+    let meter_provider = metering_provider();
+
+    let meter = meter_provider.meter("demo-app");
+    // Create a Counter Instrument.
+    let counter = meter.u64_counter("request").init();
+
     match rand::random::<u8>() {
         number @ 0..50 => {
             tracing::info!("Handler is OK. number: {number}");
+
+            counter.add(1, &[opentelemetry::KeyValue::new("status", "ok")]);
 
             Ok(())
         }
@@ -35,15 +45,21 @@ async fn handler() -> Result<(), StatusCode> {
 
             tracing::event!( name: "seconds from UNIX_EPOCH", Level::INFO, time = %time);
 
+            counter.add(1, &[opentelemetry::KeyValue::new("status", "ok")]);
+
             Ok(())
         }
         number @ 100..150 => {
             tracing::warn!("Handler is WARN, but STATUS CODE - OK. number: {number}");
 
+            counter.add(1, &[opentelemetry::KeyValue::new("status", "warn")]);
+
             Ok(())
         }
         number @ 150..200 => {
             tracing::warn!("Handler is WARN. number: {number}");
+
+            counter.add(1, &[opentelemetry::KeyValue::new("status", "warn")]);
 
             Err(StatusCode::FORBIDDEN)
         }
@@ -53,6 +69,8 @@ async fn handler() -> Result<(), StatusCode> {
             tokio::time::sleep(Duration::from_secs(1)).await;
 
             tracing::event!(Level::INFO, "Slept 1s");
+
+            counter.add(1, &[opentelemetry::KeyValue::new("status", "err")]);
 
             Err(StatusCode::INTERNAL_SERVER_ERROR)
         }
